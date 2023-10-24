778494845b99be3df3e977c3d98817816d02c5aa
Alati11 <peppealati@gmail.com>
2023-10-24 17:59:49 +0200

    error img currentindex

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ createApp({
         return {
             currentIndex: 0,
             slides: [
-                {
+                { // 0
                     image: 'img/01.webp',
-                }, {
+                }, { // 1
                     image: 'img/02.webp',
                 }, {
                     image: 'img/03.webp',
@@ -23,14 +23,14 @@ createApp({
     methods: {
         nextImage() {
             this.currentIndex++;
-            if (this.currentIndex > this.slides.image.length -1 ) {
+            if (this.currentIndex > this.currentIndex.slides.image.length-1 ) {
                 this.currentIndex = 0;
             }
         },
         prevImage() {
             this.currentIndex--;
             if (this.currentIndex < 0 ) {
-                this.currentIndex === this.slides.image.length -1;
+                this.currentIndex === this.currentIndex.slides.image.length -1;
             }        
         },
         changeImage() {
